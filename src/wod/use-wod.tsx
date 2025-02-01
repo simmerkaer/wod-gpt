fd22723304b5954cab9537557192580cb5a70a8a
Steffen Immerkær <sim@schultz.dk>
2025-02-01 13:58:45 +0100

    disable gpt call

--- src/wod/use-wod.tsx
+++ src/wod/use-wod.tsx
@@ -1,16 +1,16 @@
 import { useState, useEffect } from "react";
 
 export const useWod = (): string | null => {
-  const [data, setData] = useState<string | null>(null);
+  const [data, setData] = useState<string | null>("test wod");
 
-  useEffect(() => {
-    fetch("/api/wod")
-      .then((res) => res.text())
-      .then((text) => setData(text))
-      .catch((error) => {
-        console.error("Error fetching WOD data:", error);
-      });
-  }, []);
+  //   useEffect(() => {
+  //     fetch("/api/wod")
+  //       .then((res) => res.text())
+  //       .then((text) => setData(text))
+  //       .catch((error) => {
+  //         console.error("Error fetching WOD data:", error);
+  //       });
+  //   }, []);
 
   return data;
 };
