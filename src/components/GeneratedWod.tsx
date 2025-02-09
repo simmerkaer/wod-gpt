393e79e2d7f2df3ab1503cfcf1d231ce8bbc15f1
esow <simmerkaer@gmail.com>
2025-02-09 21:05:51 +0100

    Copy workout to clipboard

--- src/components/GeneratedWod.tsx
+++ src/components/GeneratedWod.tsx
@@ -1,11 +1,26 @@
+import { ClipboardCopy } from "lucide-react";
 import React from "react";
+import { Button } from "./ui/button";
 
 interface GeneratedWodProps {
   wod: string | null;
 }
 
 const GeneratedWod: React.FunctionComponent<GeneratedWodProps> = ({ wod }) => {
-  if (wod) return <pre className="text-left text-wrap">{wod}</pre>;
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(wod || "");
+  };
+
+  if (wod)
+    return (
+      <div>
+        <pre className="text-left text-wrap">{wod}</pre>
+        <Button variant="outline" onClick={copyToClipboard} className="mt-10">
+          <ClipboardCopy />
+          Copy workout to clipboard
+        </Button>
+      </div>
+    );
 };
 
 export default GeneratedWod;
