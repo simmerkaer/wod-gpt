02fc31d21f3690d78d085c8a4cda560aefdbca68
simmerkaer <simmerkaer@gmail.com>
2025-06-06 21:09:12 +0200

    Add close button to drawer

--- src/components/SelectMovements.tsx
+++ src/components/SelectMovements.tsx
@@ -1,5 +1,6 @@
 import { MovementId } from "@/lib/movementId";
 import movements from "@/lib/movementList";
+import { X } from "lucide-react";
 import * as React from "react";
 import { useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
@@ -13,6 +14,7 @@ import {
 } from "./ui/dialog";
 import {
   Drawer,
+  DrawerClose,
   DrawerContent,
   DrawerHeader,
   DrawerTitle,
@@ -87,8 +89,12 @@ const SelectMovements: React.FunctionComponent<SelectMovementsProps> = ({
     <Drawer>
       <DrawerTrigger asChild>{trigger}</DrawerTrigger>
       <DrawerContent>
-        <DrawerHeader>
+        <DrawerHeader className="relative mb-4">
           <DrawerTitle className="text-center"></DrawerTitle>
+          <DrawerClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2">
+            <X className="h-4 w-4" />
+            <span className="sr-only">Close</span>
+          </DrawerClose>
         </DrawerHeader>
         <ScrollArea className="overflow-y-auto px-4">
           {renderContent()}
